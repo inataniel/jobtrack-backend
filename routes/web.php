e5ba2519e29bcfd7302763e3329381e03ae3384d
Stivi Nataniel Erik <stivinatanielerik@gmail.com>
2026-01-30 16:15:55 +0100

    CSRF

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApplicationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\ApplicationController;
+Route::get('/csrf-token', function () {
+    return response()->json(['token' => csrf_token()]);
+});
 
 Route::prefix('api')->group(function () {
     Route::apiResource('applications', ApplicationController::class);
